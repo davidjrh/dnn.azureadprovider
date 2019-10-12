06c314299182ea2840a86e240735dcb28ca1b9a2
David Rodriguez <davidj@intelequia.com>
2019-10-12 12:38:32 +0100

    Updated System.IdentityModel.Tokens.Jwt reference and dependencies

--- DotNetNuke.Authentication.Azure/Components/AzureClient.cs
+++ DotNetNuke.Authentication.Azure/Components/AzureClient.cs
@@ -27,6 +27,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Web;
 using System.Web.Script.Serialization;
