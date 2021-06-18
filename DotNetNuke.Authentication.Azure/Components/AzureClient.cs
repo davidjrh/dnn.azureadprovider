66155ee92d38b654f502ec5d1e5cf2698cba4179
David Rodriguez <davidj@intelequia.com>
2021-06-18 13:02:07 +0100

    Solves #45 (Error on AuthorizationEndpoint redirect)

--- DotNetNuke.Authentication.Azure/Components/AzureClient.cs
+++ DotNetNuke.Authentication.Azure/Components/AzureClient.cs
@@ -627,7 +627,6 @@ namespace DotNetNuke.Authentication.Azure.Components
 
 
                 HttpContext.Current.Response.Redirect(AuthorizationEndpoint + "?" + parameters.ToNormalizedString(), false);
-                HttpContext.Current.Response.Flush();
                 return AuthorisationResult.RequestingCode;
             }
 
