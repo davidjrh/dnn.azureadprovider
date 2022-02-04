9c1325edf4333b93496d8acedd8bcb2f32c39490
David Rodriguez <davidj@intelequia.com>
2022-02-04 09:39:54 +0000

    Removed Azure Graph dependency, now all implemented with Microsoft Graph.

---
+++ DotNetNuke.Authentication.Azure/Components/Graph/GraphServiceClientFactory.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Graph;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace DotNetNuke.Authentication.Azure.Components.Graph
+{
+    internal static class GraphServiceClientFactory
+    {
+        public static GraphServiceClient GetAuthenticatedGraphClient(
+            Func<Task<string>> acquireAccessToken)
+        {
+            return new GraphServiceClient(
+                new CustomAuthenticationProvider(acquireAccessToken)
+            );
+        }
+    }
+
+    class CustomAuthenticationProvider : IAuthenticationProvider
+    {
+        private readonly Func<Task<string>> _acquireAccessToken;
+        public CustomAuthenticationProvider(Func<Task<string>> acquireAccessToken)
+        {
+            _acquireAccessToken = acquireAccessToken;
+        }
+
+        public async Task AuthenticateRequestAsync(HttpRequestMessage requestMessage)
+        {
+            var accessToken = await _acquireAccessToken.Invoke();
+
+            // Add the token in the Authorization header
+            requestMessage.Headers.Authorization = new AuthenticationHeaderValue(
+                "Bearer", accessToken
+            );
+        }
+    }
+}
